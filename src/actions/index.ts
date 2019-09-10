b100d9daaf460d516fe143633418364e9afade96
Evan Moon <bboydart91@gmail.com>
2019-09-10 22:52:51 +0200

    Action interface 정의

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -3,55 +3,61 @@ import { networkOptions } from '../config';
 
 const { learningRate, learningLimit, nodePerLayer, layerCount } = networkOptions;
 
-export const setLearningRate = (rate: number = learningRate) => {
+export interface ActionCreator {
+  type: string;
+  payload?: Object;
+  error?: boolean;
+};
+
+export const setLearningRate = (rate: number = learningRate): ActionCreator => {
   return {
     type: types.SET_LEARNING_RATE,
     payload: { rate },
   };
 };
 
-export const setLearningLimit = (limit: number = learningLimit) => {
+export const setLearningLimit = (limit: number = learningLimit): ActionCreator => {
   return {
     type: types.SET_LEARNING_LIMIT,
     payload: { limit },
   };
 };
 
-export const setNodePerLayer = (count: number = nodePerLayer) => {
+export const setNodePerLayer = (count: number = nodePerLayer): ActionCreator => {
   return {
     type: types.SET_NODE_PER_LAYER,
     payload: { count },
   };
 };
 
-export const setLayerCount = (count: number = layerCount) => {
+export const setLayerCount = (count: number = layerCount): ActionCreator => {
   return {
     type: types.SET_LAYER_COUNT,
     payload: { count },
   };
 };
 
-export const setNodeGraphicData = (data: { nodes: any[], links: any[] }) => {
+export const setNodeGraphicData = (data: { nodes: any[], links: any[] }): ActionCreator => {
   return {
     type: types.SET_NODE_GRAPHIC_DATA,
     payload: { data },
   };
 };
 
-export const updateErrorDataset = () => {
+export const updateErrorDataset = (): ActionCreator => {
   return {
     type: types.UPDATE_ERROR_DATASET,
   };
 };
 
-export const updateOutputDataset = () => {
+export const updateOutputDataset = (): ActionCreator => {
   return {
     type: types.UPDATE_OUTPUT_DATASET,
   };
 };
 
-export const setLearningResult = () => {
+export const setLearningResult = (): ActionCreator => {
   return {
-    type: types.SET_LEARNING_RATE,
+    type: types.SET_LEARNING_RESULT,
   };
 };
