3e3c158b5d1bd1ea459e278942b25a77ff841eaa
evan-moon <bboydart91@gmail.com>
2020-12-03 10:25:22 +0000

    Deploying to gh-pages from  @ 0d60a1e44c7804a404a7bd13882a75a6ea2f82cb 🚀

---
+++ precache-manifest.62f0a9a2a248d0c71c17e6282f70b0de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3fc3b60c5e3e8e44a31e78a20f447da",
+    "url": "/simple-ann/index.html"
+  },
+  {
+    "revision": "0bf7859f2631aebb378c",
+    "url": "/simple-ann/static/css/2.c3cb89c7.chunk.css"
+  },
+  {
+    "revision": "56d1d369fd569fbc95ed",
+    "url": "/simple-ann/static/css/main.6773f949.chunk.css"
+  },
+  {
+    "revision": "0bf7859f2631aebb378c",
+    "url": "/simple-ann/static/js/2.d52eb26a.chunk.js"
+  },
+  {
+    "revision": "56d1d369fd569fbc95ed",
+    "url": "/simple-ann/static/js/main.65b88e67.chunk.js"
+  },
+  {
+    "revision": "6ee87b7d7a8ec4b3fa39",
+    "url": "/simple-ann/static/js/runtime~main.cd6200da.js"
+  }
+]);
